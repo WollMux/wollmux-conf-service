d02785332e07f7c8a76ad454a4c75839f2b11a60
Daniel Sikeler <daniel.sikeler@muenchen.de>
2017-02-20 12:51:36 +0100

    Conf über HTTP lesen

--- conf-service/src/main/java/de/muenchen/wollmux/conf/service/camel/HttpReadProcessor.java
+++ conf-service/src/main/java/de/muenchen/wollmux/conf/service/camel/HttpReadProcessor.java
@@ -13,6 +13,14 @@ public class HttpReadProcessor extends HttpReadBinaryProcessor
   @Override
   public void process(Exchange exchange) throws Exception
   {
+    String url = exchange.getIn().getHeader("url").toString();
+    byte[] content = read(url);
+
+    if (content != null && content.length > 0)
+    {
+      exchange.getOut().setBody(new String(content));
+      exchange.getOut().setHeader("url", url);
+    }
   }
 
 }
